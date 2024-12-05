3123136739dcad39b8c13177bc0f31ac58105329
Jeff Thompson <160783290+jthompson-arcus@users.noreply.github.com>
2024-12-05 07:29:02 -0500

    Wrap Shiny bindings

--- inst/app/www/custom.js
+++ inst/app/www/custom.js
@@ -8,44 +8,52 @@ function ts(cb) {
   }
 }
 
-var customCheckbox = new Shiny.InputBinding();
+$(document).ready(function() {
+  
+  /* Define custom Shiny input binding for overall review checkbox. 
+  This is needed to assign an event priority to the checkbox.*/
+  var customCheckbox = new Shiny.InputBinding();
+  
+  $.extend(customCheckbox, {
+    find: function(scope) {
+      return $(scope).find("input[type='checkbox'].cs_checkbox");
+    },
+    getValue: function(el) {
+      return el.checked;
+    },
+    setValue: function(el, value) {
+      el.checked = value;
+    },
+    subscribe: function(el, callback) {
+      $(el).on("change.checkboxInputBinding", function() {
+        Shiny.onInputChange($(this).attr('id'), this.checked, {priority: 'event'});
+      });
+    },
+    unsubscribe: function(el) {
+      $(el).off(".checkboxInputBinding");
+    }
+  });
+  
+  Shiny.inputBindings.register(customCheckbox);
+  
+  /* Define custom Shiny output binding for review progress bar. 
+  It expects 4 values: completed, unmarking, marking, and total.*/
+  var customProgressBar = new Shiny.OutputBinding();
+  
+  $.extend(customProgressBar, {
+    find: function(scope) {
+      return $(scope).find("div.cs-progress-bar");
+    },
+    renderValue: function(el, data) {
+      let cmp_pct = (data.completed-data.unmarking)/data.total*100;
+      let um_pct = data.unmarking/data.total*100;
+      let m_pct = data.marking/data.total*100;
+      $('#' + el.id + " .cs-progress.completed").width(cmp_pct.toFixed(2) + "%")
+      $('#' + el.id + " .cs-progress.unmarking").width(um_pct.toFixed(2) + "%")
+      $('#' + el.id + " .cs-progress.marking").width(m_pct.toFixed(2) + "%")
+    }
+  });
+  
+  Shiny.outputBindings.register(customProgressBar)
 
-$.extend(customCheckbox, {
-  find: function(scope) {
-    return $(scope).find("input[type='checkbox'].cs_checkbox");
-  },
-  getValue: function(el) {
-    return el.checked;
-  },
-  setValue: function(el, value) {
-    el.checked = value;
-  },
-  subscribe: function(el, callback) {
-    $(el).on("change.checkboxInputBinding", function() {
-      Shiny.onInputChange($(this).attr('id'), this.checked, {priority: 'event'});
-    });
-  },
-  unsubscribe: function(el) {
-    $(el).off(".checkboxInputBinding");
-  }
 });
-
-Shiny.inputBindings.register(customCheckbox);
-
-var customProgressBar = new Shiny.OutputBinding();
-
-$.extend(customProgressBar, {
-  find: function(scope) {
-    return $(scope).find("div.cs-progress-bar");
-  },
-  renderValue: function(el, data) {
-    let cmp_pct = (data.completed-data.unmarking)/data.total*100;
-    let um_pct = data.unmarking/data.total*100;
-    let m_pct = data.marking/data.total*100;
-    $('#' + el.id + " .cs-progress.completed").width(cmp_pct.toFixed(2) + "%")
-    $('#' + el.id + " .cs-progress.unmarking").width(um_pct.toFixed(2) + "%")
-    $('#' + el.id + " .cs-progress.marking").width(m_pct.toFixed(2) + "%")
-  }
-});
-
-Shiny.outputBindings.register(customProgressBar)
