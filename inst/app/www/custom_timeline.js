be1bb99005b02594bd57d7aad884da6be367521c
LDSamson <l.d.samson@gmail.com>
2024-12-18 15:45:26 +0100

    Start making a temporary custom js function to redraw the timeline items

---
+++ inst/app/www/custom_timeline.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+     
+  var timevis = $("#cf_adverse_events-timeline_fig-timeline")
+  /*var timevis = $(".vis-item.vis-box")*/
+  timevis.on("select", function(){
+    console.log("click occurred"); 
+    /* WIP: here I want to trigger the `redraw` timevis method*/
+    /*https://visjs.github.io/vis-timeline/docs/timeline/*/
+    });
+    
+  });
