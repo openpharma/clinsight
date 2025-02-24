a046cb0b3528922a225d670816cea55073cd7ed2
LDSamson <l.d.samson@gmail.com>
2025-02-24 13:02:59 +0100

    Test all tables in appdata

--- tests/testthat/test-mod_review_form_tbl_fct_helpers.R
+++ tests/testthat/test-mod_review_form_tbl_fct_helpers.R
@@ -90,47 +90,64 @@ describe("get_form_table() works", {
     expect_true(grepl("<b>", hypotension_ae))
   })
   
-  it("Shows SAE-specific columns if is_SAE is TRUE and form is 'Adverse events'", {
-        appdata <- clinsightful_data[clinsightful_data$item_group == "Adverse events", ] |> 
-          get_appdata()
-        meta_vars <- get_meta_vars(appdata)
-        rev_data <- get_review_data(appdata[["Adverse events"]]) |> 
-          dplyr::mutate(
-            id = dplyr::row_number(),
-            reviewed = rep_len(c("Yes", "No"), length.out = dplyr::n()),
-            status = ifelse(reviewed == "No", "old", "new")
-          )
-        form_items <- meta_vars$items[["Adverse events"]]
-        
-        output <- get_form_table(
-          form_data = appdata[["Adverse events"]], 
-          form_review_data = rev_data, 
-          form = "Adverse events",
-          form_items = form_items,
-          active_subject = "DEU_02_482", 
-          is_reviewed = TRUE,
-          is_SAE = TRUE
-        )
-        expect_true(is.data.frame(output))
-        expect("o_reviewed" %in% names(output), "`o_reviewed` is an expected column for form data")
-        enabled_rows <- lapply(output[["o_reviewed"]], \(x) isFALSE(x$disabled)) |> unlist()
-        expect_equal(unique(output[enabled_rows,]$subject_id), "DEU_02_482")
-        
-        output_table <- output |> 
-          dplyr::select(-o_reviewed) |> 
-          lapply(\(x){if(is.character(x)) gsub("\\**<\\/*b>", "", x) else x}) |> 
-          dplyr::as_tibble() 
-        original_table <- appdata[["Adverse events"]] |> 
-          create_table(expected_columns = names(form_items)) |> 
-          dplyr::filter(grepl("Yes", `Serious Adverse Event`)) |> 
-          dplyr::select(dplyr::any_of(
-            c("o_reviewed", "subject_id","form_repeat", "Name", "AESI",  "SAE Start date",
-              "SAE End date", "CTCAE severity", "Treatment related",
-              "Treatment action", "Other action", "SAE Category",
-              "SAE Awareness date", "SAE Date of death", "SAE Death reason")
-          )) |>
-          adjust_colnames("^SAE ")
-        expect_equal(original_table, output_table)
-      })
+  it("Creates the same tables as create_table, but with review information included", {
+    appdata <- get_appdata(clinsightful_data)
+    table_names <- setNames(nm = names(appdata))
+    form_items <- get_meta_vars(appdata)$items
+    rev_data <- lapply(table_names, \(x){
+      appdata[[x]] |> 
+        get_review_data() |> 
+        dplyr::mutate(
+          id = dplyr::row_number(),
+          reviewed = rep_len(c("Yes", "No"), length.out = dplyr::n()),
+          status = ifelse(reviewed == "No", "old", "new")
+        )  
+    })
+    # ignore AE/SAE column adjustments:
+    testthat::local_mocked_bindings(
+      adjust_ae_form_table = function(data, is_SAE){data}
+    )
+    output <- lapply(table_names, \(x){
+      get_form_table(
+        form_data = appdata[[x]], 
+        form_review_data = rev_data[[x]], 
+        form = x,
+        form_items = form_items[[x]],
+        active_subject = "BEL_04_772", 
+        is_reviewed = TRUE
+      )
+    })
+    lapply(table_names, \(x){
+      cat("get_form_table | Verifying review data in form '", x, "'\n")
+      expect_true(is.data.frame(output[[!!x]]))
+      expect(
+        "o_reviewed" %in% names(output[[x]]), 
+        paste0("`o_reviewed` is an expected column for data in form `", x, "`.")
+      )
+      enabled_rows <- lapply(output[[x]][["o_reviewed"]], \(i) isFALSE(i$disabled)) |> unlist()
+      if(any(enabled_rows)){
+        expect_equal(unique(output[[!!x]][enabled_rows,]$subject_id), "BEL_04_772")
+      } else{
+        expect_true(!"BEL_04_772" %in% unique(output[[!!x]]$subject_id))
+      }
+    }) |> 
+      invisible()
+    
+    output_tables <- lapply(table_names, \(x){
+      output[[x]] |> 
+        dplyr::select(-o_reviewed) |> 
+        lapply(\(x){if(is.character(x)) gsub("\\**<\\/*b>", "", x) else x}) |> 
+        dplyr::as_tibble() 
+    })
+    original_tables <- lapply(table_names, \(x){
+      create_table(appdata[[x]], expected_columns = names(form_items[[x]])) 
+    })
+    lapply(table_names, \(x){
+      expect_equal(original_tables[[!!x]], output_tables[[!!x]])
+    })
+  })
+})
+
+describe("adjust_ae_form_table() works as expected", {
   
 })
