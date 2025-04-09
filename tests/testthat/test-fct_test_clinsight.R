52c5570e3b4716e858bec77bf605f700a4db06bc
LDSamson <l.d.samson@gmail.com>
2025-04-09 13:42:55 +0200

    Finalize function test

--- tests/testthat/test-fct_test_clinsight.R
+++ tests/testthat/test-fct_test_clinsight.R
@@ -2,9 +2,24 @@ describe("test_clinsight() works", {
   it("creates a temporary folder and temporary files for one-time use with run_app()", {
     local_mocked_bindings(
       run_app = function(...){
-        ### verify objects here
+        expect_equal(Sys.getenv("GOLEM_CONFIG_ACTIVE"), "test")
       }
     )
     expect_no_error(test_clinsight())
+  })
+  it("errors with incorrect input", {
+    expect_error(
+      test_clinsight(clinsight_data = "incorrect_argument"),
+      "Expecting a data frame"
+      )
+    expect_error(
+      test_clinsight(meta_data = "incorrect_argument"),
+      "Expecting a list"
+      )
+    expect_error(test_clinsight(clinsight_config = mtcars))
+    expect_error(
+      test_clinsight(clinsight_data = mtcars, clinsight_config = "default"),
+      "The 'default' config should not be used with custom data"
+    )
   })
 })
