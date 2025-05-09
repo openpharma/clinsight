71a5efd2928ffba306477b58bbe25187c900e137
LDSamson <l.d.samson@gmail.com>
2025-05-09 11:06:38 +0200

    Add two more feature 6 test scenarios

--- tests/testthat/test-app_feature_06.R
+++ tests/testthat/test-app_feature_06.R
@@ -73,16 +73,16 @@ describe(
           "main_sidebar_1-review_forms_1-progress_bar",
           "main_sidebar_1-review_forms_1-save_review_error"
         )
+        #snapshot 001:
         app$expect_values(input = input_names, output = output_names)
         app$click("main_sidebar_1-review_forms_1-confirm_saving")
         app$wait_for_idle()
+        #snapshot 002:
         app$expect_values(input = input_names, output = output_names)
         user_db <- app$get_value(export = "user_db")
+        
         active_form_data <- db_get_table(user_db) |> 
-          dplyr::filter(
-            subject_id == app$get_value(export = "active_participant"),
-            item_group == app$get_value(export = "active_form")
-          )
+          dplyr::filter(item_group == app$get_value(export = "active_form"))
         expect_equal(unique(active_form_data$reviewed), "Yes")
         expect_equal(unique(active_form_data$reviewer), c("test user (Administrator)"))
       }
@@ -129,8 +129,133 @@ describe(
           "main_sidebar_1-review_forms_1-progress_bar",
           "main_sidebar_1-review_forms_1-save_review_error"
         )
+        #snapshot 003:
         app$expect_values(input = input_names, output = output_names)
       }
     )
+    it(
+      "Scenario 3 - Revert to subject level review and review a subject. 
+            Given the state of scenario 2, 
+            and reverting to subject-level review,
+            I expect that I now only see the data from subject BEL_04_772 again,
+            and that when I click on [form_reviewed] and then save,
+            no warning message is displayed, 
+            and all Adverse Event form data of BEL_04_772 is saved correctly as being reviewed.", 
+      {
+        app$set_inputs("main_sidebar_1-review_forms_1-review_type" = "subject")
+        app$wait_for_idle(1000)
+        
+        expect_equal(app$get_js(sprintf(count_SAE_checkboxes, "")), 0)
+        expect_equal(app$get_js(sprintf(count_AE_checkboxes, "")), 5)
+        expect_equal(app$get_js(sprintf(count_AE_checkboxes, ":not(:checked)")), 5)
+        
+        expect_equal(app$get_value(input = "cf_adverse_events-show_all_data"), FALSE)
+        expect_equal(app$get_value(output = "form_level_review"), FALSE)
+        
+        app$set_inputs("main_sidebar_1-review_forms_1-form_reviewed" = TRUE)
+        app$wait_for_idle()
+        #All visible checkboxes should now be checked:
+        expect_equal(app$get_js(sprintf(count_AE_checkboxes, ":checked")), 5)
+        
+        app$click("main_sidebar_1-review_forms_1-save_review")
+        app$wait_for_idle()
+        user_db <- app$get_value(export = "user_db")
+        active_form_data <- db_get_table(user_db) |> 
+          dplyr::filter(item_group == app$get_value(export = "active_form"))
+        
+        expect_equal(
+          unique(with(active_form_data, reviewed[subject_id == "BEL_04_772"])), 
+          "Yes"
+        )
+        expect_equal(
+          unique(with(active_form_data, reviewer[subject_id == "BEL_04_772"])),
+          "test user (Administrator)"
+        )
+        # Other rows in the database of the Adverse Events remain not reviewed:
+        expect_equal(
+          unique(with(active_form_data, reviewed[subject_id != "BEL_04_772"])), 
+          "No"
+        )
+        input_names <- c(
+          "cf_adverse_events-review_form_tbl-table_review_selection",
+          "cf_adverse_events-review_form_SAE_tbl-table_review_selection",
+          "main_sidebar_1-review_forms_1-confirm_saving"
+        )
+        output_names <-c(
+          "form_level_review",
+          "main_sidebar_1-navigate_forms_1-form_name",
+          "main_sidebar_1-review_forms_1-form_reviewed",
+          "main_sidebar_1-review_forms_1-progress_bar",
+          "main_sidebar_1-review_forms_1-save_review_error"
+        )
+        #snapshot 004:
+        app$expect_values(input = input_names, output = output_names)
+      }
+    )
+    it(
+      "Scenario 4 - Form level review of a study form. 
+            Given the state of scenario 3, 
+            and browsing to Vital signs
+            and reverting to form-level review,
+            and clicking on form_review,
+            I expect that I now see all vital signs data,
+            and that when I click on [form_reviewed] and then save,
+            and confirm the save in a warning message,
+            that all data from the vital signs form is saved correctly as being reviewed.", 
+      {
+        app$set_inputs(main_tabs = "Study data", common_data_tabs = "Vital signs")
+        app$wait_for_idle(1000)
+        app$set_inputs("main_sidebar_1-review_forms_1-review_type" = "form")
+        app$wait_for_idle(1000)
+        
+        expect_equal(app$get_js(sprintf(count_VS_checkboxes, "")), 68)
+        expect_equal(app$get_js(sprintf(count_VS_checkboxes, ":not(:checked)")), 68)
+        expect_equal(app$get_js(sprintf(count_VS_checkboxes, ":not(:disabled)")), 68)
+        
+        expect_equal(app$get_value(input = "sf_vital_signs-show_all"), TRUE)
+        expect_equal(app$get_value(output = "form_level_review"), TRUE)
+        
+        app$set_inputs("main_sidebar_1-review_forms_1-form_reviewed" = TRUE)
+        app$wait_for_idle()
+        #All checkboxes should now be checked:
+        
+        expect_equal(app$get_js(sprintf(count_VS_checkboxes, ":checked")), 68)
+        
+        app$click("main_sidebar_1-review_forms_1-save_review")
+        app$wait_for_idle()
+        # Warning/confirmation should show up:
+        modal_text <- app$get_js('$("#main_sidebar_1-review_forms_1-confirm_save_modal").text()') |> 
+          gsub(pattern = " *\n +", replacement = " ")
+        expect_true(
+          grepl(
+            "This will change the review status of ALL items in the form Vital signs to reviewed",
+            modal_text
+          )
+        )
+        input_names <- c(
+          "sf_vital_signs-review_form_tbl-table_review_selection",
+          "main_sidebar_1-review_forms_1-confirm_saving"
+        )
+        output_names <-c(
+          "form_level_review",
+          "main_sidebar_1-navigate_forms_1-form_name",
+          "main_sidebar_1-review_forms_1-form_reviewed",
+          "main_sidebar_1-review_forms_1-progress_bar",
+          "main_sidebar_1-review_forms_1-save_review_error"
+        )
+        #snapshot 005:
+        app$expect_values(input = input_names, output = output_names)
+        app$click("main_sidebar_1-review_forms_1-confirm_saving")
+        app$wait_for_idle()
+        #snapshot 006:
+        app$expect_values(input = input_names, output = output_names)
+        user_db <- app$get_value(export = "user_db")
+        
+        active_form_data <- db_get_table(user_db) |> 
+          dplyr::filter(item_group == app$get_value(export = "active_form"))
+        expect_equal(unique(active_form_data$reviewed), "Yes")
+        expect_equal(unique(active_form_data$reviewer), c("test user (Administrator)"))
+      }
+    ) 
   }
 )
