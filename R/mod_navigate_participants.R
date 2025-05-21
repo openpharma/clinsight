61637d3448df75543bdf0e858d0d414a350e7868
LDSamson <l.d.samson@gmail.com>
2025-05-21 18:03:21 +0200

    Slightly more robust, will not crash if study status is missing
    (cherry picked from commit 97d0421a2c7b0d031bf9f2c72f16c964ae2d6566)

--- R/mod_navigate_participants.R
+++ R/mod_navigate_participants.R
@@ -137,7 +137,8 @@ mod_navigate_participants_server <- function(id, r){
         )
       } else{
         active_pt_info <- r$filtered_tables$General |> 
-          dplyr::filter(subject_id == r$subject_id)
+          subset(subject_id == r$subject_id) |> 
+          add_missing_columns("study_status")
         list(
           pt_info = paste0(active_pt_info$Sex, ", ", active_pt_info$Age, "yrs."),
           status_icon = switch(
