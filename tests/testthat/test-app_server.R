1f2781a86197fd235a30e57824ce5817f4240ed5
LDSamson <l.d.samson@gmail.com>
2025-12-15 18:30:51 +0100

    Fix expected test message in app_server test

--- tests/testthat/test-app_server.R
+++ tests/testthat/test-app_server.R
@@ -127,8 +127,8 @@ describe(
         session$flushReact()
         expect_equal(
           user_error(),
-          paste0("No user name provided. Functionality is limited. ",
-                 "If this is unexpected, please contact the administrator. ")
+          paste0("No valid user name provided. Functionality is limited. ",
+                 "If this is unexpected, please contact the administrator.")
         )
       }, 
       session = app_session) |> 
