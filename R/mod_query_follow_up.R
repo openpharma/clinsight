361daf0f1bc4f54e484a8631594b1a7365e35f70
LDSamson <l.d.samson@gmail.com>
2026-01-06 09:01:29 +0100

    Ensure on the server that query is not yet resolved before adding a FU message

--- R/mod_query_follow_up.R
+++ R/mod_query_follow_up.R
@@ -47,24 +47,21 @@ mod_query_follow_up_server <- function(id, r, selected_query, db_path){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
     
+    is_resolved <- reactiveVal()
     observeEvent(selected_query(), {
-      is_resolved <- any(
+      is_resolved(any(
         with(r$query_data, resolved[query_id == selected_query()]) == "Yes" 
-      )
-      shiny::updateCheckboxInput(inputId = "resolved", value = is_resolved)
+      ))
+      shiny::updateCheckboxInput(inputId = "resolved", value = is_resolved())
       shiny::updateTextAreaInput(
         inputId = "query_follow_up_text", 
         placeholder = ifelse(
-          is_resolved, 
+          is_resolved(), 
           "query is resolved", 
           "add response here"
         )
       )
-      if(is_resolved){
-        shinyjs::disable("query_follow_up") 
-      } else{
-        shinyjs::enable("query_follow_up")
-      }
+      shinyjs::toggleState("query_follow_up", condition = isFALSE(is_resolved()))
     })
     
     allowed_to_resolve <- reactive({
@@ -76,6 +73,7 @@ mod_query_follow_up_server <- function(id, r, selected_query, db_path){
     observeEvent(input$query_add_follow_up, {
       req(input$query_follow_up_text, r$user_name, r$user_role, selected_query())
       req(selected_query() %in% r$query_data$query_id)
+      req(isFALSE(is_resolved()))
       query_save_error(FALSE)
       golem::cat_dev("Query FU text to add: ", input$query_follow_up_text, "\n")
       ts <- time_stamp()
